ce5b27b8d3e3336c486ae7032765efcfba8ecb6c
Umidcoding <abdurahmonovumidjon500@gmail.com>
2025-02-01 15:51:53 +0500

    calculator

---
+++ js/script.js
@@ -0,0 +1,71 @@
+const display = document.getElementById('display');
+let buttons = document.querySelectorAll('.cal__btn');
+let currentValue = "";
+let operators = ['+', '-', '*', '/']
+let isResultDisplayed = false;
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let value = e.target.textContent.trim();
+
+
+    if (value === "Back") { 
+      currentValue = currentValue.slice(0, -1);
+      display.innerText = currentValue.length === 0 ? "0" : currentValue;
+      return;
+    }
+
+    if (value === "C") {
+      display.innerText = "0";
+      currentValue = "";
+      isResultDisplayed = false
+      return;
+    }
+
+    if (operators.includes(value)) {
+      if (currentValue === "" || operators.includes(currentValue.slice(-1))) {
+        return;
+      }
+    }
+
+
+    if(isResultDisplayed && !operators.includes(value)) {
+      display.innerText = value;
+      currentValue = value;
+      isResultDisplayed = false;
+      return;
+    }
+
+
+    if (value === "=") {
+      showResult();
+      return;
+    }
+
+    if (display.innerText === "0" || currentValue == "") {
+      display.innerText = value;
+      currentValue = value;
+    } else {
+      display.innerText += value;
+      currentValue += value;
+    }
+  });
+});
+
+function showResult() {
+  let expression = currentValue;
+
+  if (expression === "" || operators.includes(expression.slice(-1))) {
+    return;
+  }
+
+  try {
+    let result = eval(expression);
+    display.innerText = result;
+    currentValue = result.toString(); 
+    isResultDisplayed = true
+  } catch (error) {
+    display.innerText = "Error";
+    currentValue = "";
+  }
+}
